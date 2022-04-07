1b5fd6872e731565f921ef580e0bb59e19f8287b
Taylor Summers <taylor7summers@gmail.com>
2022-04-07 13:00:05 -0700

    enhanced firestore helper for polling

--- api-tests/helpers/firestore.helper.ts
+++ api-tests/helpers/firestore.helper.ts
@@ -3,6 +3,9 @@ import {Firestore, Timestamp} from '@google-cloud/firestore';
 import {EnvConfig} from '../config/envConfig.model';
 // eslint-disable-next-line node/no-unpublished-import
 import {Chance} from 'chance';
+import AxiosHelper from './axiosClient.helper';
+import {BookResponse} from '../models/response/book.get.response.model';
+import {poll} from './misc.helper';
 
 class FirestoreHelper {
   private firestore = new Firestore();
@@ -10,9 +13,17 @@ class FirestoreHelper {
   private firestoreName = FirestoreHelper.envConfig.firestoreCollection;
   private chance = new Chance();
 
+  private axiosValidateFailure = (status: any) => {
+    return status < 500;
+  };
+
   private store = this.firestore.collection(this.firestoreName);
 
-  public addBookData = async (isbn: string, language = 'English') => {
+  public addBookData = async (
+    isbn: string,
+    language = 'English',
+    _waitForBook = false
+  ) => {
     const batch = this.firestore.batch();
     const doc = this.store.doc(isbn);
 
@@ -28,10 +39,25 @@ class FirestoreHelper {
     try {
       await batch.commit();
       console.log('Saved books in Firestore');
+      if (_waitForBook) {
+        await this.waitForBook(isbn);
+      }
     } catch (e) {
       console.error('Error saving books:', e);
       return;
     }
+  };
+
+  public waitForBook = async (isbn: string) => {
+    const fn = () =>
+      AxiosHelper.get<BookResponse>(
+        `books/${isbn}/`,
+        {},
+        false,
+        this.axiosValidateFailure
+      );
+    const fnCondition = (result: {status: number}) => result.status !== 200;
+    return await poll(fn, fnCondition, 1000);
   };
 }
 
---
+++ api-tests/specs/books.delete.spec.ts
@@ -0,0 +1,22 @@
+import {AxiosResponse} from 'axios';
+import AxiosHelper from '../helpers/axiosClient.helper';
+import FirestoreHelper from '../helpers/firestore.helper';
+import {BookResponseData} from '../models/response/book.post.response.model';
+
+describe('DELETE /books/isbn', () => {
+  describe('Success Flows', () => {
+    const isbn = '9788338003412';
+    const firestore = new FirestoreHelper();
+
+    beforeAll(async () => {
+      await firestore.addBookData(isbn, 'English', true);
+    });
+
+    it('Should respoond with ', async () => {
+      const {data, status}: AxiosResponse<BookResponseData> =
+        await AxiosHelper.delete<BookResponseData>(`books/${isbn}`);
+      expect(status).toBe(204);
+      console.log(data);
+    });
+  });
+});
